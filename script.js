db997457023b957d51581675356621d0fcf141c1
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2019-11-02 19:25:37 -0700

    Created UI for user to enter number to convert to roman numeral

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+$(function() {
+  // Only allow number input, enter, and backspace
+  $('input').on('keypress', function(evt) {
+    if ( !(evt.key === 'Enter' || evt.key === 'Backspace' || (evt.key >= 0 && evt.key <= 9)) ) {
+      evt.preventDefault();
+    }
+  });
+
+  $('form').on('submit', function(evt) {
+    evt.preventDefault();
+    convert( $('input').val() );
+  });
+
+  $('#clear').on('click', function(evt) {
+    $('#result').empty();
+    $('input').focus();
+  });
+});
+
+
 function convert(number) {
   let roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
   let value = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
@@ -14,5 +34,7 @@ function convert(number) {
     }
   }
 
-  return array.join('');
+  $('#result').append(`<li>${$('input').val()} = ${array.join('')}</li>`);
+
+  $('input').val('');
 }
